05c525e3ffaf4230b4176e10f72dc62dc30a31f1
pmaths1 <malingrey@maths1.fr>
2020-02-24 11:50:23 +0100

    Add files via upload
    depôt du 23/02

---
+++ Maalov-Mansour-Maïwand/mansour_truc_chose.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int fonction(int a)
+
+{
+
+		cout << "x=";
+		cout << a << std::endl;
+		return 0;
+
+}
+
+
+int main()
+{
+
+    cout << fonction(5) << std::endl;
+    return 0;
+
+}
+
+
+
